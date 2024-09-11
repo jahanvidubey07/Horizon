ec1edcfd849c851bb9bdae7c5fd18ecd83c7f7b4
jahanvi dubey <dubeyjahanvi07@gmail.com>
2024-09-11 18:46:30 +0530

    implimenting dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,55 +24,68 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.action'
+import PlaidLink from './PlaidLink'
 
 
-   const AuthForm = ({ type }: { type: string }) => {
-    const router = useRouter();
-    const [user, setUser] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
+const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formSchema = authFormSchema(type);
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        email: "",
+        password: ''
+      },
+    })
    
-  
-    const formSchema = authFormSchema(type);
-
-      // 1. Define your form.
-     const form = useForm<z.infer<typeof formSchema>>({
-     resolver: zodResolver(formSchema),
-     defaultValues: {
-      email: "",
-      password: ''
-    },
-  })
+   
  
   // 2. Define a submit handler.
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-      try {
-        //Sign up with Appwrite & create plaid token 
-        if(type === 'sign-up'){
-         const newUser = await signUp(data);
 
-         setUser(newUser);
-
-
-        }
-
-        if(type === 'sign-in'){
-           const response = await signIn({
-            email: data.email,
-            password: data.password,
-          })
-          if(response) router.push('/')   
-        }
-        
-      } catch (error) {
-        console.log(error);
-      }finally{
-        setIsLoading(false);
-      }
+    try {
+      // Sign up with Appwrite & create plaid token
       
+      if(type === 'sign-up') {
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+
+        const newUser = await signUp(userData);
+
+        setUser(newUser);
+      }
+
+      if(type === 'sign-in') {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        })
+
+        if(response) router.push('/')
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
-
-
+  }
+  
   return (
     <section className="auth-form">
     <header className='flex flex-col gap-5 md:gap-8'>
@@ -104,12 +117,12 @@ import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.action'
           </div>
       </header>
       
-        {user ? (
+      {user ? ( 
             <div className='flex flex-col gap-4'>
-                {/*PlaidLink */}
+                <PlaidLink  user = {user} variant = "primary"/>
 
             </div>
-        ):(
+       ):( 
             <>
      <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -230,7 +243,7 @@ import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.action'
       </Link>
     </footer>
             </>
-        )}
+        )} 
 
     </section>
   )
